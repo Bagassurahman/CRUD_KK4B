1d8ccd69a032d797c621e8fcf2788377f4134f53
Bagassurahman <bagassurahman2@gmail.com>
2020-11-21 20:33:34 +0700

    crud

---
+++ resources/js/Jetstream/Apps.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>Hello {{ $page.user.name }}, ini halaman Dashboard!</h2>
+    </div>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="overflow-hidden shadow-xl sm:rounded-lg">
+                    <apps />
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    import Apps from '@/Jetstream/Apps'
+
+    export default {
+        components: {
+            AppLayout,
+            Apps,
+        },
+    }
+
+</script>
